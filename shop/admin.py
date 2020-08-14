3b73292df392d0fd293b4f034446db9377eb7f02
Dave <davidalejandrodand@gmail.com>
2020-08-14 18:44:26 -0400

    add csv export admin function

--- shop/admin.py
+++ shop/admin.py
@@ -24,6 +24,27 @@ def make_refund_accepted(modeladmin, request, queryset):
 
 make_refund_accepted.short_description = 'Update orders to refund granted'
 
+def export_to_csv(modeladmin, request, queryset):
+	opts = modeladmin.model._meta
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+	writer = csv.writer(response)
+
+	fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+	# Write a first row with header information
+	writer.writerow([field.verbose_name for field in fields])
+	# Write data rows
+	for obj in queryset:
+		data_row = []
+		for field in fields:
+			value = getattr(obj, field.name)
+			if isinstance(value, datetime.datetime):
+				value = value.strftime('%d/%m/%Y')
+			data_row.append(value)
+		writer.writerow(data_row)
+	return response
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 
@@ -95,7 +116,7 @@ class OrderAdmin(admin.ModelAdmin):
 		'user__username',
 		'ref_code',
 	]
-	actions = [make_refund_accepted]
+	actions = [make_refund_accepted, export_to_csv]
 
 
 #admin.site.register(OrderAdmin)
