e2b29dc75964a554af1e41e5124d9bc7a07f278d
Dave <davidalejandrodand@gmail.com>
2020-08-18 13:32:41 -0400

    modified permited host

--- settings/prod.py
+++ settings/prod.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://electronixshop.herokuapp.com/']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
